568858ad545229433b5dadb9f55c348481809d1e
Real Mohsin <realmohsin@outlook.com>
2020-10-06 03:58:53 -0400

    gatsby-img and svg components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require(`path`)
+
 module.exports = {
   siteMetadata: {
     title: 'Business Lawyer New York - Entertainment Lawyer NYC',
@@ -5,5 +7,16 @@ module.exports = {
       "Romano Law's innovative New York lawyers focused in Business Law, Entertainment Law and Sports Law. Learn why business owners and creatives choose Romano.",
     author: '' // this is for twitter seo, use twitter handle
   },
-  plugins: ['gatsby-plugin-react-helmet']
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Layout from '../components/Layout'
+import Logo from '../svg-components/Logo'
 
-export default function Home () {
+export default function Home ({ data }) {
   return (
     <Layout>
+      <Img fixed={data.file.childImageSharp.fixed} alt={'Romano Law Logo'} />
+      <Logo />
       <div>Hello world!</div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "eyes.png" }) {
+      childImageSharp {
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
+
+// export const query = graphql`
+//   query {
+//     logo: file(relativePath: { eq: "eyes.png" }) {
+//       childImageSharp {
+//         fluid(quality: 90, maxWidth: 4160) {
+//           ...GatsbyImageSharpFluid_withWebp
+//         }
+//       }
+//     }
+//   }
+// `
